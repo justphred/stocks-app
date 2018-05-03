980796d84a45ea5d4f133a31f8964d08dc79b97e
fcraft <fcraft@southern-tech.com>
2018-05-03 12:02:33 -0400

    First stab at initializing state and passing props

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class TickerItem extends Component {
   render() {
     return (
       <li style={{display: "inline-block",
-                  padding:"1em",
+                  "padding-top":".25em",
+                  "padding-bottom":".25em",
+                  "padding-right":"1em",
+                  "padding-left":"1em",
                   margin: ".5em",
                   "background-color": "#c5cce2",
                   "border-radius": ".45em",
-                }}>Ticker Value</li>
+                  border: "1px solid #343b49"
+                }}>
+                  <p>{this.props.symbol}</p>
+                  <p>{this.props.value}</p>
+                </li>
     )
   }
 }
 
 class TickerItemList extends Component {
   render () {
+    let tickerItems = this.props.items.map( (item, ndx) => {
+      return (
+        <TickerItem symbol={item.symbol} value={item.currentValue} />
+      );
+    } );
     return (
       <div>
         <ul>
-          <TickerItem />
-          <TickerItem />
-          <TickerItem />
-          <TickerItem />
-          <TickerItem />
+          {tickerItems}
         </ul>
       </div>
     );
@@ -32,6 +40,25 @@ class TickerItemList extends Component {
 }
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      stockItems: [
+        { symbol: "ABC",
+          currentValue: 123 },
+        { symbol: "DEF",
+          currentValue: 345 },
+        { symbol: "HIJK",
+          currentValue: 7 },
+        { symbol: "LOM",
+          currentValue: 1123 },
+        { symbol: "FRT",
+          currentValue: 23 }
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -42,7 +69,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <TickerItemList />
+        <TickerItemList items={ this.state.stockItems }/>
       </div>
     );
   }
